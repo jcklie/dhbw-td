5cd0660217091a3eb0e264832fd2d2159a552382
Martin Kießling <kiesslingmartin91@googlemail.com>
2013-04-04 09:35:46 +0200

    added Wave package and classes

---
+++ dhbw-td/core/src/main/java/de/dhbw/td/core/waves/WaveController.java
@@ -0,0 +1,33 @@
+/*  Copyright (C) 2013 by Martin Kieﬂling, Inc. All rights reserved.
+ *  Released under the terms of the GNU General Public License version 3 or later.
+ *  
+ *  Contributors:
+ *  Martin Kieﬂling - All
+ */
+
+package de.dhbw.td.core.waves;
+
+import java.util.Queue;
+
+public class WaveController {
+
+	public final Queue<Wave> waves;
+	public final int numberOfWaves;
+	public final double timeSinceLastWave;
+	public final Wave currentWave;
+	
+	public WaveController(Queue<Wave> waves, int numberOfWaves, double timeSinceLastWave, Wave currentWave){
+		this.waves = waves;
+		this.numberOfWaves = numberOfWaves;
+		this.timeSinceLastWave = timeSinceLastWave;
+		this.currentWave = currentWave;
+	}
+	
+	public void nextWave(){
+				
+	}
+	
+	public void draw(){
+		
+	}
+}
