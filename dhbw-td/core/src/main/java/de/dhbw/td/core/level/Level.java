901032eac76cd147f1a68f0765534a1ac1bd2d0c
Sebastian Muszytowski <sebastian@muszytowski.net>
2013-04-08 20:22:58 +0200

    Add waypoint support.

--- dhbw-td/core/src/main/java/de/dhbw/td/core/level/Level.java
+++ dhbw-td/core/src/main/java/de/dhbw/td/core/level/Level.java
@@ -3,11 +3,14 @@
  *  
  *  Contributors:
  *  Jan-Christoph Klie - All
+ *  Sebastian Muszytowski - Add waypoint support
  */
 
 package de.dhbw.td.core.level;
 
+import java.awt.Point;
 import java.util.Arrays;
+import java.util.Queue;
 
 import playn.core.Image;
 import playn.core.Surface;
@@ -19,11 +22,13 @@ public class Level implements IDrawable{
 	public final int width;
 	public final int tilesize;
 	public final Image[][] map;
+	public final Queue<Point> waypoints;
 
-	public Level(Image[][] map, int tilesize, int width, int height) {
+	public Level(Image[][] map, Queue<Point> waypoints, int tilesize, int width, int height) {
 		this.tilesize = tilesize;
 		this.width = width;
 		this.height = height;
+		this.waypoints = waypoints;
 			
 		if( map.length != height) {
 			throw new IllegalArgumentException("Specified height differs from height of map array!");
--- dhbw-td/core/src/main/java/de/dhbw/td/core/level/SimpleLevelFactory.java
+++ dhbw-td/core/src/main/java/de/dhbw/td/core/level/SimpleLevelFactory.java
@@ -2,22 +2,35 @@
  *  Released under the terms of the GNU General Public License version 3 or later.
  *  
  *  Contributors:
- *  Jan-Christoph Klie - All
+ *  Jan-Christoph Klie - Basic Structure and First Implementation
+ *  Sebastian Muszytowski - Waypoint Queue and refactoring
  */
 
 package de.dhbw.td.core.level;
 
 import static playn.core.PlayN.assets;
 import static playn.core.PlayN.json;
+
+import java.awt.Point;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import playn.core.Image;
 import playn.core.Json;
 
 /**
  * 
  * @author Jan-Christoph Klie
- *
+ * @author Sebastian Muszytowski *
  */
 public class SimpleLevelFactory implements ILevelFactory {
+	
+	private enum Direction {
+		RIGHT,
+		LEFT,
+		UP,
+		DOWN;
+	}
 	
 	private enum ETileType {	
 		
@@ -65,19 +78,42 @@ public class SimpleLevelFactory implements ILevelFactory {
 			this.resourceName = resourceName;
 		}
 	}
+	
+	private int width;
+	private int height;
+	private int tilesize;
+	private int startx;
+	private int starty;
+	private Json.Array grid;
+	
+	private void init(Json.Object parsedJson){
+		width = parsedJson.getInt("width");
+		height = parsedJson.getInt("height");
+		tilesize = parsedJson.getInt("tilesize");
+		startx = parsedJson.getInt("startx");
+		starty = parsedJson.getInt("starty");
+		grid = parsedJson.getArray("tiles");
+	}
+	
 
 	@Override
 	public Level loadLevel(String jsonString) {
-		return loadLevel(json().parse(jsonString));
+		Json.Object configuration = json().parse(jsonString);
+		return loadLevel(configuration);
 	}
 
 	@Override
 	public Level loadLevel(Json.Object parsedJson) {
-		int width = parsedJson.getInt("width");
-		int height = parsedJson.getInt("height");
-		int tilesize = parsedJson.getInt("tilesize");
+		init(parsedJson);
+
+		Image[][] tileMap = loadTileMap();
+		System.out.println(tileMap);
+		Queue<Point> waypoints = generateWaypoints();
 		
-		Json.Array grid = parsedJson.getArray("tiles");
+		return new Level(tileMap,waypoints, tilesize, width, height);
+	}
+	
+	private Image[][] loadTileMap(){
 		Image[][] tileMap = new Image[height][width];
 		
 		for(int row = 0; row < height; row++) {
@@ -89,8 +125,143 @@ public class SimpleLevelFactory implements ILevelFactory {
 				tileMap[row][col] = assets().getImage(pathToImage);
 			}
 		}
-		return new Level(tileMap, tilesize, width, height);
+		
+		return tileMap;
 	}
 	
+	/**
+	 * Method to generate waypoints from map data. It utilizes
+	 * the start and end specified in the json file and calcuates the
+	 * route to end. In case of unresolvable issues it returns an error.
+	 * 
+	 * @return Queue A queue consisting of points.
+	 */
+	private Queue<Point> generateWaypoints() {
+		int x = startx;
+		
+		if(x != 0){
+			throw new IllegalArgumentException("Sorry, the map should start somewhere in x=0");
+		}
+		
+		int y = starty;
+		Queue<Point> queue = new LinkedList<Point>();
+		queue.add(new Point(x,y));
+				
+		ETileType curTile = getTileType(x, y);
+		Direction direction = Direction.RIGHT;
+		
+		while(curTile != ETileType.PATH_END){
+			switch(curTile){
+			case GRID:
+				throw new IllegalStateException("GRID is bad: "+x+","+y);
+			case EDGE_LEFT_BOTTOM:
+				if(direction == Direction.LEFT){
+					queue.add(new Point(x,y));
+					direction = Direction.UP;
+					y--;
+				}else if(direction == Direction.DOWN){
+					queue.add(new Point(x,y));
+					direction = Direction.RIGHT;
+					x++;
+				}else{
+					throw new IllegalStateException("Cannot send enemy through wall at: "+x+","+y);
+				}
+				break;
+			case EDGE_LEFT_TOP:
+				if(direction == Direction.LEFT){
+					queue.add(new Point(x,y));
+					direction = Direction.DOWN;
+					y++;
+				}else if(direction == Direction.UP){
+					queue.add(new Point(x,y));
+					direction = Direction.RIGHT;
+					x++;
+				}else{
+					throw new IllegalStateException("Cannot send enemy through wall at: "+x+","+y);
+				}
+				break;
+			case EDGE_RIGHT_BOTTOM:
+				if(direction == Direction.RIGHT){
+					queue.add(new Point(x,y));
+					direction = Direction.UP;
+					y--;
+				}else if(direction == Direction.DOWN){
+					queue.add(new Point(x,y));
+					direction = Direction.LEFT;
+					x--;
+				}else{
+					throw new IllegalStateException("Cannot send enemy through wall at: "+x+","+y);
+				}
+				break; 
+			case EDGE_RIGHT_TOP:
+				if(direction == Direction.RIGHT){
+					queue.add(new Point(x,y));
+					direction = Direction.DOWN;
+					y++;
+				}else if(direction == Direction.UP){
+					queue.add(new Point(x,y));
+					direction = Direction.LEFT;
+					x--;
+				}else{
+					throw new IllegalStateException("Cannot send enemy through wall at: "+x+","+y);
+				}
+				break;
+				// keep the current direction in this case	
+				case PATH_EMPTY:
+					if(direction == Direction.LEFT){
+						x--;
+					}else if(direction == Direction.RIGHT){
+						x++;
+					}else if(direction == Direction.DOWN){
+						y++;
+					}else if(direction == Direction.UP){
+						y--;
+					}
+				break;
+			case PATH_HORIZONTAL:
+				if(direction == Direction.LEFT){
+					x--;
+				}else if(direction == Direction.RIGHT){
+					x++;
+				}else{
+					throw new IllegalStateException("Illegal waypoint detected (not left or right) at: "+x+","+y);
+				}
+				break;
+				// we force the map to start with one step to the right
+			case PATH_START:
+				direction = Direction.RIGHT;
+				x++;
+				break;
+			case PATH_VERTICAL:
+				if(direction == Direction.DOWN){
+					y++;
+				}else if(direction == Direction.UP){
+					y--;
+				}else{
+					throw new IllegalStateException("Illegal waypoint detected (not up or down) at: "+x+","+y);
+				}
+				break;
+			default:
+				throw new IllegalStateException("Illegal waypoint detected at: "+x+","+y);
+			
+			}
+			
+			// set tile to next tile
+			curTile = getTileType(x, y);
+			
+			// check whether we are still in boundary. Else raise error
+			if(x < 0 || y < 0 || x > width-1 || y > height-1){
+				throw new IllegalStateException("Illegal map file. Waypoints leaving boundaries at: "+x+","+y);
+			}
+			
+		}
+		// here we should have reached the end if not we're unlucky
+		queue.add(new Point(x,y));
+		return queue;
+	}
+	
+	private ETileType getTileType(int x,int y){
+		return ETileType.createFromTileId(grid.getArray(y).getInt(x));
+	}
 
 }
