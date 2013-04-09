752862f277356d139b07688d9b89b47353aab852
Martin Kießling <kiesslingmartin91@googlemail.com>
2013-04-09 11:39:28 +0200

    mm

---
+++ dhbw-td/core/src/main/java/de/dhbw/td/core/level/SimpleLevelFactory.java
@@ -0,0 +1,96 @@
+/*  Copyright (C) 2013 by Jan-Christoph Klie, Inc. All rights reserved.
+ *  Released under the terms of the GNU General Public License version 3 or later.
+ *  
+ *  Contributors:
+ *  Jan-Christoph Klie - All
+ */
+
+package de.dhbw.td.core.level;
+
+import static playn.core.PlayN.assets;
+import static playn.core.PlayN.json;
+import playn.core.Image;
+import playn.core.Json;
+
+/**
+ * 
+ * @author Jan-Christoph Klie
+ *
+ */
+public class SimpleLevelFactory implements ILevelFactory {
+	
+	private enum ETileType {	
+		
+		GRID("grid.bmp"),
+		WHITE("white.bmp"),
+		EDGE_LEFT_BOTTOM("edge_left_bottom.bmp"),
+		EDGE_LEFT_TOP("edge_left_top.bmp"),
+		EDGE_RIGHT_BOTTOM("edge_right_bottom.bmp"),
+		EDGE_RIGHT_TOP("edge_right_top.bmp"),
+		PATH_EMPTY("path_empty.bmp"),
+		PATH_HORIZONTAL("path_horizontal.bmp"),
+		PATH_VERTICAL("path_vertical.bmp"),
+		PATH_START("start.bmp"),
+		PATH_END("finish.bmp"),
+		TOWER_DUMMY("code.png");
+		
+		public final String resourceName;
+
+		private static final String pathToTiles = "tiles";
+		
+		public static String getPathToImage(int tileID) {
+			ETileType tileType = createFromTileId(tileID);
+			return String.format("%s/%s", pathToTiles, tileType.resourceName);
+		}
+		
+		private static ETileType createFromTileId(int id) {
+			switch(id) {
+			case 0: return GRID;
+			case 1: return WHITE;
+			case 2: return EDGE_LEFT_BOTTOM;
+			case 3: return EDGE_LEFT_TOP;
+			case 4: return EDGE_RIGHT_BOTTOM;
+			case 5: return EDGE_RIGHT_TOP;
+			case 6: return PATH_EMPTY;
+			case 7: return PATH_HORIZONTAL;
+			case 8: return PATH_VERTICAL;
+			case 9: return PATH_START;
+			case 10: return PATH_END;
+			case 11: return TOWER_DUMMY;
+			default: throw new IllegalArgumentException("No ETileType with Tile ID:" + id);
+			}			
+		}
+
+		ETileType(String resourceName) {
+			this.resourceName = resourceName;
+		}
+	}
+
+	@Override
+	public Level loadLevel(String jsonString) {
+		return loadLevel(json().parse(jsonString));
+	}
+
+	@Override
+	public Level loadLevel(Json.Object parsedJson) {
+		int width = parsedJson.getInt("width");
+		int height = parsedJson.getInt("height");
+		int tilesize = parsedJson.getInt("tilesize");
+		
+		Json.Array grid = parsedJson.getArray("tiles");
+		Image[][] tileMap = new Image[height][width];
+		
+		for(int row = 0; row < height; row++) {
+			Json.Array gridRow = grid.getArray(row);
+			
+			for(int col = 0; col < width; col++) {
+				int tileID = gridRow.getInt(col);
+				String pathToImage = ETileType.getPathToImage(tileID);
+				tileMap[row][col] = assets().getImage(pathToImage);
+			}
+		}
+		return new Level(tileMap, tilesize, width, height);
+	}
+	
+
+}
