8ab016fab69ee13a4883485091af68219acd5c7e
Jan-Christoph Klie <rentier42@googlemail.com>
2013-04-21 02:43:17 +0200

    Fix secret

--- dhbwtd/core/src/main/java/de/dhbw/td/core/secret/CheatModule.java
+++ dhbwtd/core/src/main/java/de/dhbw/td/core/secret/CheatModule.java
@@ -3,21 +3,21 @@ package de.dhbw.td.core.secret;
 import static playn.core.PlayN.assets;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.List;
 
-import de.dhbw.td.backup.Button;
-import de.dhbw.td.backup.GameState;
-import de.dhbw.td.backup.HUD;
-import de.dhbw.td.backup.IKeyboardObserver;
-import de.dhbw.td.backup.TowerDefense;
-
 import playn.core.Image;
+import playn.core.Keyboard;
 import playn.core.Keyboard.Event;
+import playn.core.Keyboard.Listener;
+import playn.core.Keyboard.TypedEvent;
 import playn.core.Sound;
+import de.dhbw.td.core.TowerDefense;
+import de.dhbw.td.core.game.GameState;
+import de.dhbw.td.core.ui.Button;
+import de.dhbw.td.core.ui.HUD;
 
 
-public class CheatModule implements IKeyboardObserver {
+public class CheatModule implements Keyboard.Listener{
 		
 	private static final int MAX_BUFFER_SIZE = 25;
 	
@@ -35,27 +35,6 @@ public class CheatModule implements IKeyboardObserver {
 		TowerDefense.getKeyboard().addObserver(this);
 		
 		king = assets().getImageSync("tower/king.png");
-	}
-
-	@Override
-	public void alert(Event e) {		
-		if(e.key().toString().length() > 1) {
-			return;
-		}
-		
-		buffer.append(e.key());
-		
-		if( buffer.length() > MAX_BUFFER_SIZE) {
-			buffer.deleteCharAt(0);
-		}
-		
-		System.out.println(buffer);
-		
-		try {
-			handleCheatz();
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
 	}
 	
 	private boolean in(String s) {
@@ -91,6 +70,40 @@ public class CheatModule implements IKeyboardObserver {
 		mySound.play();
 		mySound.setLooping(true);
 		buffer.delete(0, buffer.length());
+	}
+
+	@Override
+	public void onKeyDown(Event e) {
+		if(e.key().toString().length() > 1) {
+			return;
+		}
+		
+		buffer.append(e.key());
+		
+		if( buffer.length() > MAX_BUFFER_SIZE) {
+			buffer.deleteCharAt(0);
+		}
+		
+		System.out.println(buffer);
+		
+		try {
+			handleCheatz();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void onKeyTyped(TypedEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onKeyUp(Event event) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
