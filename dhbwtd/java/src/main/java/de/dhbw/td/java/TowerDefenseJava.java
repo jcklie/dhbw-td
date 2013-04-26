66e9e794ecb42166fe41faaa17824b47151381f9
Jan-Christoph Klie <rentier42@googlemail.com>
2013-04-26 23:05:52 +0200

    iconz

--- dhbwtd/java/src/main/java/de/dhbw/td/java/TowerDefenseJava.java
+++ dhbwtd/java/src/main/java/de/dhbw/td/java/TowerDefenseJava.java
@@ -7,6 +7,18 @@
 
 package de.dhbw.td.java;
 
+import java.awt.Image;
+import java.awt.image.PixelGrabber;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import javax.imageio.ImageIO;
+
+import org.lwjgl.opengl.Display;
+
 import playn.core.PlayN;
 import playn.java.JavaPlatform;
 import playn.java.JavaPlatform.Config;
@@ -14,7 +26,7 @@ import de.dhbw.td.core.TowerDefense;
 
 public class TowerDefenseJava {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Config config = new Config();
 	    config.width = 896;
 	    config.height = 640;	    
@@ -25,6 +37,57 @@ public class TowerDefenseJava {
 		platform.graphics().registerFont("Miso", "fonts/miso.otf");
 		
 		TowerDefense towerDefense = new TowerDefense();
+		
+		ByteBuffer[] list = new ByteBuffer[2];
+		list[0] = createBuffer( getImage("/icons/DHTD_32.bmp") );
+		list[1] = createBuffer( getImage("/icons/DHTD_32.bmp") );
+		Display.setIcon(list);
+		
 		PlayN.run(towerDefense);
+		
+	}
+	
+	/**
+	 * I do not handle the exception since I cannot recover from it
+	 * @param pathToImage
+	 * @return
+	 */
+	private static Image getImage(String pathToImage) {
+		
+		try { 
+			System.out.println(pathToImage);
+			InputStream stream = TowerDefenseJava.class.getResourceAsStream(pathToImage);
+			return ImageIO.read(stream);
+		} catch (IOException e) { throw new RuntimeException("Cannot find image: " + pathToImage); }
+	}
+	
+	private static ByteBuffer createBuffer(Image img) {
+		int height = (int) img.getHeight(null);
+		int width = (int) img.getWidth(null);
+		int len = width * height;
+		ByteBuffer temp = ByteBuffer.allocateDirect(len << 2);
+
+		temp.order(ByteOrder.LITTLE_ENDIAN);
+
+		int[] pixels = new int[len];
+
+		PixelGrabber pg = new PixelGrabber( img, 0, 0, width, height, pixels, 0, width);
+
+		try {
+			pg.grabPixels();
+		} catch (InterruptedException e) {
+
+		}
+
+		for (int i = 0; i < len; i++) {
+			int pos = i << 2;
+			int texel = pixels[i];
+			if (texel != 0) {
+				texel |= 0xff000000;
+			}
+			temp.putInt(pos, texel);
+		}
+
+		return temp;
 	}
 }
