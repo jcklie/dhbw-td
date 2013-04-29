bf1f074497f2fb61749098c69f22366a8a8f2a19
Jan-Christoph Klie <rentier42@googlemail.com>
2013-04-29 13:02:22 +0200

    remove icon

--- dhbwtd/java/src/main/java/de/dhbw/td/java/TowerDefenseJava.java
+++ dhbwtd/java/src/main/java/de/dhbw/td/java/TowerDefenseJava.java
@@ -7,17 +7,7 @@
 
 package de.dhbw.td.java;
 
-import java.awt.Image;
-import java.awt.image.PixelGrabber;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-
-import javax.imageio.ImageIO;
-
-import org.lwjgl.opengl.Display;
 
 import playn.core.PlayN;
 import playn.java.JavaPlatform;
@@ -38,56 +28,9 @@ public class TowerDefenseJava {
 		
 		TowerDefense towerDefense = new TowerDefense();
 		
-		ByteBuffer[] list = new ByteBuffer[2];
-		list[0] = createBuffer( getImage("/icons/DHTD_32.bmp") );
-		list[1] = createBuffer( getImage("/icons/DHTD_32.bmp") );
-		Display.setIcon(list);
 		
 		PlayN.run(towerDefense);
 		
 	}
 	
-	/**
-	 * I do not handle the exception since I cannot recover from it
-	 * @param pathToImage
-	 * @return
-	 */
-	private static Image getImage(String pathToImage) {
-		
-		try { 
-			System.out.println(pathToImage);
-			InputStream stream = TowerDefenseJava.class.getResourceAsStream(pathToImage);
-			return ImageIO.read(stream);
-		} catch (IOException e) { throw new RuntimeException("Cannot find image: " + pathToImage); }
-	}
-	
-	private static ByteBuffer createBuffer(Image img) {
-		int height = (int) img.getHeight(null);
-		int width = (int) img.getWidth(null);
-		int len = width * height;
-		ByteBuffer temp = ByteBuffer.allocateDirect(len << 2);
-
-		temp.order(ByteOrder.LITTLE_ENDIAN);
-
-		int[] pixels = new int[len];
-
-		PixelGrabber pg = new PixelGrabber( img, 0, 0, width, height, pixels, 0, width);
-
-		try {
-			pg.grabPixels();
-		} catch (InterruptedException e) {
-
-		}
-
-		for (int i = 0; i < len; i++) {
-			int pos = i << 2;
-			int texel = pixels[i];
-			if (texel != 0) {
-				texel |= 0xff000000;
-			}
-			temp.putInt(pos, texel);
-		}
-
-		return temp;
-	}
 }
