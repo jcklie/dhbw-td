7b824b12c0a1071cef998c04c6c74595d610fbe6
Jan-Christoph Klie <rentier42@googlemail.com>
2013-05-08 20:55:36 +0200

    Comment foo

--- dhbwtd/core/src/main/java/de/dhbw/td/core/fsm/IAction.java
+++ dhbwtd/core/src/main/java/de/dhbw/td/core/fsm/IAction.java
@@ -1,5 +1,17 @@
+/*  Copyright (C) 2013. All rights reserved.
+ *  Released under the terms of the GNU General Public License version 3 or later.
+ *  
+ *  Contributors:
+ *  Jan-Christoph Klie - All 
+ */
+
 package de.dhbw.td.core.fsm;
 
+/**
+ * Defines an action, also know as callback or anonymous function. It is ugly but java
+ * has no other way of defining them.
+ *
+ */
 public interface IAction {
 	
 	IAction NONE = new IAction() {
